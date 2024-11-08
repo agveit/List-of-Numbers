4acee1854d8de82f91a9d723db8c28925991339c
agveit <agveit.a@mail.ru>
2024-11-08 16:02:09 +0500

    complete work

--- List of Numbers/MainWindow.xaml.cs
+++ List of Numbers/MainWindow.xaml.cs
@@ -1,28 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace List_of_Numbers
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click_Delete(object sender, RoutedEventArgs e)
+        {
+            numberList.Items.Remove(numberList.SelectedItem);
+        }
+
+        // Переменная для запоминания последнего введённого числа
+        int clickC = 0;
+        
+        
+        private void Button_Click_Add(object sender, RoutedEventArgs e)
+        {
+            numberList.Items.Add(clickC);
+            clickC++;
+        }
+
+        // Обработчик для очистки списка и переменной 
+        private void Button_Click_Refresh(object sender, RoutedEventArgs e)
+        {
+            clickC = 0;
+            numberList.Items.Clear();
         }
     }
 }
